564c49962e391410ac76afc849a9c3cbd6aa8210
Neel Smith <nsmith@Technites.local>
2020-06-20 09:05:28 -0400

    Start implementing

--- jvm/src/main/scala/markupindexer/TEIpersNameIndexer.scala
+++ jvm/src/main/scala/markupindexer/TEIpersNameIndexer.scala
@@ -7,9 +7,54 @@ import edu.holycross.shot.scm._
 import edu.holycross.shot.xmlutils._
 import scala.xml._
 
+import scala.annotation.tailrec
 
 object TEIpersNameIndexer extends MidMarkupIndexer {
 
   def verb = Cite2Urn("urn:cite2:cite:verbs.v1:appearsIn")
-  def indexedNode(cn: CitableNode): Set[CiteTriple] = Set.empty[CiteTriple]
+
+  def indexedNames(context: CtsUrn, n: xml.Node, names: Vector[IndexedName] = Vector.empty[IndexedName]) : Vector[IndexedName]= {
+    n match {
+      case t: xml.Text =>  {
+        names
+      }
+
+      case e: xml.Elem =>  {
+        e.label match {
+          case "persName" => {
+            println("FOUND ONE! ")
+            try {
+              val urn = e.attribute("n").head.text
+              // CHANGE TO USE COLLECT TEXT FORM XML UTILS
+              val persName = TextReader.collectText(e)
+              println("it's " + urn + " " + persName)
+              val indexedName = IndexedName(context, Cite2Urn(urn), persName)
+              val augmented = names :+ indexedName
+              println("RETURN AUG " + augmented)
+              augmented
+
+            } catch {
+              case t: Throwable => {
+                println("FAILED on " + e)
+                names
+              }
+            }
+            // get urn, get text, create IndexedName
+            names
+          }
+          case _ => {
+            for (ch <- e.child) {
+              indexedNames(context, ch, names)
+            }
+          }
+        }
+      }
+     }
+     names
+  }
+
+  def indexedNode(cn: CitableNode): Set[CiteTriple] = {
+    val root = XML.loadString(cn.text)
+    Set.empty[CiteTriple]
+  }
 }
--- jvm/src/test/scala/markupindexer/TeiPersNameIndexerSpec.scala
+++ jvm/src/test/scala/markupindexer/TeiPersNameIndexerSpec.scala
@@ -1,20 +1,21 @@
-package edu.holycross.shot.mid.markupreader
+package edu.holycross.shot.mid.markupindexer
 import org.scalatest.FlatSpec
 
 import edu.holycross.shot.cite._
 import edu.holycross.shot.ohco2._
 
+import scala.xml._
 
 class TeiPersNameIndexerSpec extends FlatSpec {
 
-  "The TeiPersNameIndexer object" should "recursively collect text from XML containers"  in  pending /*{
-    val xml = "<div n=\"1\"><ab>Text 1</ab></div>"
-    // expect trailing space so xml elements are
-    // not concatenated
-    val expected = "Text 1 "
-    val actual = MidProseABReader.diplomatic(xml)
-    assert(actual == expected)
-  }*/
+  "The TeiPersNameIndexer object" should "recursively collect find persName elements in XML containers"  in  {
+    val xml = """<div n="1"><ab>Text including name of <persName n="urn:cite2:dummy:test.v1:1">Agamemnon</persName></ab></div>"""
+    val root = XML.loadString(xml)
+    val actual = TEIpersNameIndexer.indexedNames(CtsUrn("urn:cts:dummy:madeUp.v1:1"),root)
+    println("ACTUAL:\n" + actual)
+
+    //assert(actual.size == 1)
+  }
 
 
 
